4049174f6fa13414ffa89c219bda993fb2b7aff6
soslab_lin <soslab_lin@soslab-desktop-24.04>
2025-02-18 23:51:47 -0500

    finish first simple float stonefish simulation

---
+++ float_rise_bringup/launch/bringup_simulation.launch.py
@@ -0,0 +1,62 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PythonExpression
+import time
+
+
+
+
+def generate_launch_description():
+    arg_robot_name = 'float_rise'
+    robot_bringup = arg_robot_name + '_bringup'
+
+    # simulation
+    simulation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','simulation.launch.py')]),
+        launch_arguments = {'arg_robot_name': arg_robot_name}.items()    
+    )
+
+    # localization = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','localization.launch.py')]),
+    #     launch_arguments = {'arg_robot_name': arg_robot_name}.items()  
+    # )
+    
+    # description = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','description.launch.py')]),
+    #     launch_arguments = {'arg_robot_name': arg_robot_name}.items()  
+    # )
+
+    # mvp_control = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','mvp_control.launch.py')]),
+    #     launch_arguments = {'arg_robot_name': arg_robot_name}.items()  
+    # )
+
+    # mvp_mission = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','mvp_mission.launch.py')]),
+    #     launch_arguments = {'arg_robot_name': arg_robot_name}.items()  
+    # )
+
+    # joy = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','joy.launch.py')]),
+    #     launch_arguments = {'arg_robot_name': arg_robot_name}.items()  
+    # )
+
+    # usbl_test = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(robot_bringup), 'launch','include','usbl.launch.py')]),
+    #     launch_arguments = {'arg_robot_name': arg_robot_name}.items()  
+    # )
+
+    return LaunchDescription([
+        simulation,
+        # localization,
+        # description,
+        # mvp_control,
+        # mvp_mission,
+        # usbl_test
+        # joy
+    ])
---
+++ float_rise_bringup/launch/include/simulation.launch.py
@@ -0,0 +1,89 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    robot_name = 'float_rise'
+    robot_bringup = robot_name + '_bringup'
+    
+    sim_world = 'float_test.scn'
+
+    world_of_stonefish_dir = get_package_share_directory('world_of_stonefish')
+
+    simulation_data = os.path.join(world_of_stonefish_dir, 'data/')
+    scenario_desc = os.path.join(world_of_stonefish_dir, 'world', sim_world)
+    simulation_rate = "100"
+    window_res_x = "800"
+    window_res_y = "800"
+    rendering_quality ="high"
+
+    robot_param_path = os.path.join(
+        get_package_share_directory(robot_bringup),
+        'config'
+        )
+
+    stonefish_driver_param_file = os.path.join(robot_param_path, 'sim_params.yaml') 
+
+    return LaunchDescription([
+        # simulation node
+        Node(
+            package="stonefish_ros2",
+            executable="stonefish_simulator",
+            name="stonefish_simulator",
+            # output="screen",
+            arguments=[simulation_data, scenario_desc, simulation_rate, window_res_x, window_res_y, rendering_quality]
+        ),
+
+        # stonefish IMU convector
+        Node(
+            package="world_of_stonefish",
+            executable="imu_driver_node",
+            namespace=robot_name,
+            name="imu_driver_node",
+            remappings=[
+                    ('imu_in/data', 'stonefish/imu/data'),
+                    ('imu_out/data', 'imu/data'),
+            ],
+            parameters=[
+                {'frame_id': robot_name + '/imu_sf'},
+                stonefish_driver_param_file
+            ]
+        ),
+
+        # stonefish thruster convector
+        Node(
+            package="world_of_stonefish",
+            executable="thruster_driver_node",
+            namespace=robot_name,
+            name="thruster_driver_node",
+            # prefix=['stdbuf -o L'],
+            # output="screen",
+            parameters=[stonefish_driver_param_file]
+        ),
+
+        # stonefish pressure convector
+        Node(
+            package="world_of_stonefish",
+            executable="pressure_sensor_node",
+            namespace=robot_name,
+            name="pressure_sensor_node",
+            parameters=[
+                {'frame_id': robot_name + '/world'}]
+        )
+
+        # stonefish usbl convector
+        # Node(
+        #     package="world_of_stonefish",
+        #     executable="usbl_driver_node",
+        #     namespace=robot_name,
+        #     name="usbl_driver_node",
+        #     # parameters=[
+        #         # {'frame_id': robot_name + '/world'}
+        #         # ]
+        # )
+
+    ])
